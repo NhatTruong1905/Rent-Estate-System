37adbabb51b0ee62d15ab40fea3618f907b3c7be
icondenimStore <tn696199@gmail.com>
2026-03-18 08:03:51 +0700

    update: Fix DTO & name's input of building-list

--- building-management-system/src/main/java/com/javaweb/dto/BuildingDTO.java
+++ building-management-system/src/main/java/com/javaweb/dto/BuildingDTO.java
@@ -1,5 +1,8 @@
 package com.javaweb.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuildingDTO {
 	private Integer id;
 	private String name;
@@ -12,7 +15,151 @@ public class BuildingDTO {
 	private String managerName;
 	private String managerPhoneNumber;
 	private Integer numberOfBasement;
-//	private List<String> typeCode = new ArrayList<String>();
+	private String structure;
+	private String floorArea;
+	private String direction;
+	private String level;
+	private String serviceFee;
+	private String carFee;
+	private String motorbikeFee;
+	private String overtimeFee;
+	private String electricityfee;
+	private String waterFee;
+	private String deposit;
+	private String payment;
+	private String rentTime;
+	private String decorationTime;
+	private String brokerageFee;
+	private String note;
+	private List<String> typeCode = new ArrayList<String>();
+
+	public String getStructure() {
+		return structure;
+	}
+
+	public void setStructure(String structure) {
+		this.structure = structure;
+	}
+
+	public String getFloorArea() {
+		return floorArea;
+	}
+
+	public void setFloorArea(String floorArea) {
+		this.floorArea = floorArea;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getServiceFee() {
+		return serviceFee;
+	}
+
+	public void setServiceFee(String serviceFee) {
+		this.serviceFee = serviceFee;
+	}
+
+	public String getCarFee() {
+		return carFee;
+	}
+
+	public void setCarFee(String carFee) {
+		this.carFee = carFee;
+	}
+
+	public String getMotorbikeFee() {
+		return motorbikeFee;
+	}
+
+	public void setMotorbikeFee(String motorbikeFee) {
+		this.motorbikeFee = motorbikeFee;
+	}
+
+	public String getOvertimeFee() {
+		return overtimeFee;
+	}
+
+	public void setOvertimeFee(String overtimeFee) {
+		this.overtimeFee = overtimeFee;
+	}
+
+	public String getElectricityfee() {
+		return electricityfee;
+	}
+
+	public void setElectricityfee(String electricityfee) {
+		this.electricityfee = electricityfee;
+	}
+
+	public String getWaterFee() {
+		return waterFee;
+	}
+
+	public void setWaterFee(String waterFee) {
+		this.waterFee = waterFee;
+	}
+
+	public String getDeposit() {
+		return deposit;
+	}
+
+	public void setDeposit(String deposit) {
+		this.deposit = deposit;
+	}
+
+	public String getPayment() {
+		return payment;
+	}
+
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+
+	public String getRentTime() {
+		return rentTime;
+	}
+
+	public void setRentTime(String rentTime) {
+		this.rentTime = rentTime;
+	}
+
+	public String getDecorationTime() {
+		return decorationTime;
+	}
+
+	public void setDecorationTime(String decorationTime) {
+		this.decorationTime = decorationTime;
+	}
+
+	public String getBrokerageFee() {
+		return brokerageFee;
+	}
+
+	public void setBrokerageFee(String brokerageFee) {
+		this.brokerageFee = brokerageFee;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
 
 	public Integer getNumberOfBasement() {
 		return numberOfBasement;
@@ -94,12 +241,12 @@ public class BuildingDTO {
 		this.managerPhoneNumber = managerPhoneNumber;
 	}
 
-//	public List<String> getTypeCode() {
-//		return typeCode;
-//	}
-//
-//	public void setTypeCode(List<String> typeCode) {
-//		this.typeCode = typeCode;
-//	}
+	public List<String> getTypeCode() {
+		return typeCode;
+	}
+
+	public void setTypeCode(List<String> typeCode) {
+		this.typeCode = typeCode;
+	}
 
 }
